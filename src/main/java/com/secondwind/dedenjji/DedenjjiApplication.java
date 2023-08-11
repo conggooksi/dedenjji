14eef75282a7e33c740d3a0000106b7d8594c10d
geonkuk <geonkuk@atlasnetworks.co.kr>
2023-08-11 09:44:29 +0900

    Initial commit

---
+++ src/main/java/com/secondwind/dedenjji/DedenjjiApplication.java
@@ -0,0 +1,13 @@
+package com.secondwind.dedenjji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DedenjjiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DedenjjiApplication.class, args);
+	}
+
+}
